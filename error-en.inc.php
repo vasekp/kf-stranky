273f9a14a08fe1b06344035eb8ef842b6ce2f7f4
Vašek Potoček <vasek.potocek@post.cz>
2020-02-10 11:36:23 +0100

    Error messages redirecting to GitHub issues

--- error-en.inc.php
+++ error-en.inc.php
@@ -1,1 +1,5 @@
-        <span class="warn">Page not found. Please continue by choosing one of the menu items.</span>
+        <span class="warn">
+          Page not found. Please continue by choosing one of the menu items.
+          <br/>
+          If you think this is an error, please report it at <a href="https://github.com/vasekp/kf-stranky/issues" target="_blank">GitHub</a>.
+        </span>
