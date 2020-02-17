0bc5631f4ef2d2d7a0452096cbe419cec1a3af9c
Vašek Potoček <vasek.potocek@post.cz>
2020-02-17 10:48:08 +0100

    Link to tutorials

--- classes.inc.php
+++ classes.inc.php
@@ -8,4 +8,4 @@
         <i><a href="https://www.databazeknih.cz/knihy/linearni-operatory-v-kvantove-fyzice-199159"
         target="_blank">Lineární operátory v kvantové fyzice</a></i> (Karolinum, Praha, 1993).
         <p></p>
-        Cvičení (vedoucí <a href="https://physics.fjfi.cvut.cz/cb-profile/jexmicha" target="_blank">RNDr. Ing. Michal Jex, Ph.D.</a>) budou v <b>pátky</b> od <b>9:30</b> v místnosti <b>B11</b>.
+        Cvičení (vedoucí <a href="https://physics.fjfi.cvut.cz/cb-profile/jexmicha" target="_blank">RNDr. Ing. Michal Jex, Ph.D.</a>, stránky cvičení <a href="https://physics.fjfi.cvut.cz/studium/predmety/292-02kfa" target="_blank">zde</a>) budou v <b>pátky</b> od <b>9:30</b> v místnosti <b>B11</b>.
