297232cdaeb68a01f165d06da1110850313c4c10
Vašek Potoček <vasek.potocek@post.cz>
2020-02-18 17:29:27 +0100

    CSS improvements

--- pub-header-cz.inc.php
+++ pub-header-cz.inc.php
@@ -1,6 +1,6 @@
         <h1>Seznam publikací</h1>
         <div class="switch hide" id="pub-filter">
-          <a id="selected">Vybrané</a>
-          <a id="recent">Nedávné</a>
-          <a id="all">Všechny</a>
+          <a id="selected" href="#">Vybrané</a>
+          <a id="recent" href="#">Nedávné</a>
+          <a id="all" href="#">Všechny</a>
         </div>
