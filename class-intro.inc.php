29a18bcdefa2108044e1466fa47e2825908c38c7
Vašek Potoček <vasek.potocek@post.cz>
2020-02-18 18:58:56 +0100

    Lecture summaries, first version

---
+++ class-intro.inc.php
@@ -0,0 +1,21 @@
+        <h1>Kvantová fyzika <span class="smaller">(02KFA)</span></h1>
+        Přednášky budou probíhat v letním semestru:
+        <ul>
+          <li>v <b>pondělí</b> od <b>16:30</b> v <b>B10</b>,</li>
+          <li>ve <b>čtvrtky</b> od <b>9:30</b> v <b>B11</b>.</li>
+        </ul>
+        Literatura k přednášce: J. Blank, P. Exner, M. Havlíček –
+        <i><a href="https://www.databazeknih.cz/knihy/linearni-operatory-v-kvantove-fyzice-199159"
+        target="_blank">Lineární operátory v kvantové fyzice</a></i> (Karolinum, Praha, 1993).
+        <p></p>
+        Cvičení (vedoucí <a href="https://physics.fjfi.cvut.cz/cb-profile/jexmicha" target="_blank">RNDr. Ing. Michal Jex, Ph.D.</a>) budou v <b>pátky</b> od <b>9:30</b> v místnosti <b>B11</b>.
+
+        <h2>Aktuality</h2>
+        <b>Upozornění studentům:</b> předmět 01KF není nadále součástí <a href="http://geraldine.fjfi.cvut.cz/bk/index.php?type=P&file=NMS_MF_1.doc&lang=cz" target="_blank">studijních plánů</a>, správný kód je 02KFA.
+        V KOS nedošlo ke zrušení starého kódu vlivem administrativní chyby.
+        Zkontrolujte prosím svůj zápis a případně kontaktujte Studijní oddělení ohledně možnosti opravy.
+
+        <div class="buttons">
+          <a class="button" href="?notes">Zápis z hodin</a>
+          <a class="button" href="https://physics.fjfi.cvut.cz/studium/predmety/292-02kfa" target="_blank">Stránky cvičení</a>
+        </div>
