7de9970c6cc8ac89a68d2a97f8681012c8e2bcec
Vašek Potoček <vasek.potocek@post.cz>
2020-02-23 12:28:50 +0100

    Use absolute paths in error document

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 include 'shared.inc.php';
 
 $curr = basename($_SERVER['SCRIPT_FILENAME'], '.php');
-if($curr == 'error')
+if($curr == 'error') {
   $en = (strpos($_SERVER['REDIRECT_QUERY_STRING'], 'l=en') !== false);
-else
+  $addr_prefix = dirname($_SERVER['PHP_SELF']) . '/';
+} else {
   $en = array_key_exists('l', $_GET) && $_GET['l'] == 'en';
+  $addr_prefix = '';
+}
 
 if($en) {
   $prilang = 'en';
@@ -33,12 +36,12 @@ $filename = $curr . ($en ? '-en' : '') . '.inc.php';
   <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width"/>
-    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="<?php print $addr_prefix; ?>css/style.css"/>
 <?php
 if(file_exists('css/' . $curr . '.css'))
-  print_indent(2, '<link rel="stylesheet" type="text/css" href="css/' . $curr . '.css"/>');
+  print_indent(2, '<link rel="stylesheet" type="text/css" href="' . $addr_prefix . 'css/' . $curr . '.css"/>');
 if(file_exists($curr . '.js'))
-  print_indent(2, '<script type="text/javascript" src="' . $curr . '.js"></script>');
+  print_indent(2, '<script type="text/javascript" src="' . $addr_prefix . $curr . '.js"></script>');
 ?>
     <title>
       Václav Potoček
@@ -49,7 +52,7 @@ if(file_exists($curr . '.js'))
 <?php
 foreach($stranky as $name => $text) {
   print_indent(3, '<span class="hide">[</span>');
-  print_indent(3, '<a href="' . query($name . '.php') . '"'
+  print_indent(3, '<a href="' . $addr_prefix . query($name . '.php') . '"'
     . ($name==$curr ? ' class="emph">' : '>') . $text . '</a>');
   print_indent(3, '<span class="hide">]</span>');
 }
@@ -100,8 +103,8 @@ print_indent(5, '<a href="' . query('', $g) . '">');
 ?>
             <span class="hide">Switch language:</span>
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="30" height="20">
-              <image href="images/<?php echo $prilang; ?>.svg" x="0" y="0" width="100%" height="100%" class="primary"/>
-              <image href="images/<?php echo $seclang; ?>.svg" x="0" y="0" width="100%" height="100%" class="secondary"/>
+              <image href="<?php print $addr_prefix; ?>images/<?php echo $prilang; ?>.svg" x="0" y="0" width="100%" height="100%" class="primary"/>
+              <image href="<?php print $addr_prefix; ?>images/<?php echo $seclang; ?>.svg" x="0" y="0" width="100%" height="100%" class="secondary"/>
             </svg>
           </a>
         </div>
