08ebefdf767c816b0c496634e2289cd72c5451be
Vašek Potoček <vasek.potocek@post.cz>
2020-03-12 10:05:33 +0100

    Class intro update

--- class-intro.inc.php
+++ class-intro.inc.php
@@ -1,22 +1,20 @@
         <h1>Kvantová fyzika <span class="smaller">(02KFA)</span></h1>
-        Přednášky budou probíhat v <b>pondělí</b> ve dvou dvouhodinových blocích
-        <b>15:30 – 17:10</b> a <b>17:30 – 19:10</b> v místnosti <b>B10</b>.
-        Čas přestávky se může dle aktuální potřeby mírně přizpůsobit.
-        <p></p>
-        Cvičení (vedoucí <a href="https://physics.fjfi.cvut.cz/cb-profile/jexmicha" target="_blank">RNDr. Ing. Michal Jex, Ph.D.</a>) budou od stejného týdne ve <b>čtvrtky</b> od <b>9:30</b> v místnosti <b>B11</b>.
-        <p></p>
-        Konzultační hodiny ideálně ve <b>středu</b> od <b>13:30</b> – komu se trvale nehodí, dle individuální domluvy.
+        <div class="strikeout">
+          Přednášky budou probíhat v <b>pondělí</b> ve dvou dvouhodinových blocích
+          <b>15:30 – 17:10</b> a <b>17:30 – 19:10</b> v místnosti <b>B10</b>.
+          Čas přestávky se může dle aktuální potřeby mírně přizpůsobit.
+          <p></p>
+          Cvičení (vedoucí <a href="https://physics.fjfi.cvut.cz/cb-profile/jexmicha" target="_blank">RNDr. Ing. Michal Jex, Ph.D.</a>) budou od stejného týdne ve <b>čtvrtky</b> od <b>9:30</b> v místnosti <b>B11</b>.
+          <p></p>
+          Konzultační hodiny ideálně ve <b>středu</b> od <b>13:30</b> – komu se trvale nehodí, dle individuální domluvy.
+        </div>
         <p></p>
         Literatura k přednášce: J. Blank, P. Exner, M. Havlíček –
         <i><a href="https://www.databazeknih.cz/knihy/linearni-operatory-v-kvantove-fyzice-199159"
         target="_blank">Lineární operátory v kvantové fyzice</a></i> (Karolinum, Praha, 1993).
 
         <h2>Aktuality</h2>
-        <b>Upozornění studentům:</b> předmět 01KF není nadále součástí <a href="http://geraldine.fjfi.cvut.cz/bk/index.php?type=P&amp;file=NMS_MF_1.doc&amp;lang=cz" target="_blank">studijních plánů</a>, správný kód je 02KFA.
-        V KOS nedošlo ke zrušení starého kódu vlivem administrativní chyby.
-        Zkontrolujte prosím svůj zápis a případně kontaktujte Studijní oddělení ohledně možnosti opravy.
-        <p></p>
-        <b>Změny rozvrhu:</b> S platností od pondělí 2.3. platí nový rozvrh v záhlaví stránky.
+        V souladu s požadavkem rektora a vedoucího katedry bude výuka po dobu přerušení studia v důsledku epidemické krize probíhat formou <b>sdílení poznámek k samostudiu</b>. Materiály budou k nalezení na této stránce.
 
         <div class="buttons">
           <a class="button" href="<?php echo query('', array('s' => 'notes')); ?>">Zápis z hodin</a>
