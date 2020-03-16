1c4fe98a01f54aeefb80d0012b4dcdabc2fbe8fd
Vašek Potoček <vasek.potocek@post.cz>
2020-03-16 20:03:37 +0100

    Class material download (WIP)

--- class-intro.inc.php
+++ class-intro.inc.php
@@ -11,6 +11,25 @@
         target="_blank">Lineární operátory v kvantové fyzice</a></i> (Karolinum, Praha, 1993).</p>
         <h2>Aktuality</h2>
         <p>V souladu s požadavkem rektora a vedoucího katedry bude výuka po dobu přerušení studia v důsledku epidemické krize probíhat formou <b>sdílení poznámek k samostudiu</b>. Materiály budou k nalezení na této stránce.</p>
+        <h2>Ke stažení</h2>
+        <table>
+          <thead>
+            <td>Datum</td>
+            <td>Soubor</td>
+            <td>Popis</td>
+          </thead>
+<?php
+$sql = 'select filename, description, timestamp from download';
+$result = $db->query($sql);
+while($row = $result->fetch_assoc()) {
+  print_indent(5, '<tr>');
+  print_indent(6, '<td>' . date('j. n. Y', strtotime($row['timestamp'])) . '</td>');
+  print_indent(6, '<td><a href="download/' . $row['filename'] . '">' . $row['filename'] . '</a></td>');
+  print_indent(6, '<td>' . $row['description'] . '</td>');
+  print_indent(5, '</tr>');
+}
+?>
+        </table>
         <div class="buttons">
           <a class="button" href="<?php echo query('', array('s' => 'notes')); ?>">Zápis z hodin</a>
           <a class="button" href="https://physics.fjfi.cvut.cz/studium/predmety/292-02kfa" target="_blank">Stránky cvičení</a>
