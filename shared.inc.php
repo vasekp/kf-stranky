c922a1c3ee215131ea786357f202b8a49e3e90af
Vašek Potoček <vasek.potocek@post.cz>
2020-03-19 14:02:38 +0100

    Better formatting of elements spread over multiple line

--- shared.inc.php
+++ shared.inc.php
@@ -21,26 +21,30 @@ function indent($text) {
   $out = '';
   foreach($arr as $line) {
     $diff = 0;
+    $line = trim($line);
     preg_match_all('/<(.)/', $line, $matches);
     foreach($matches[1] as $m) {
+      $diff++;
       if($m == '!') continue;
       if($m == '/') $diff--;
       else $diff++;
     }
-    preg_match_all('/\/>/', $line, $matches);
-    foreach($matches[0] as $m) {
+    preg_match_all('/(\/)?>/', $line, $matches);
+    foreach($matches[1] as $m) {
       $diff--;
+      if($m == '/') $diff--;
     }
-    if($diff < 0)
+    if($diff < 0 && $line[0] == '<') {
       $in += $diff;
+      $diff = 0;
+    }
     if($in < 0) {
       if($debug)
         $out .= '<!-- INDENT ERR: in < 0 -->';
       $in = 0;
     }
-    $out .= str_repeat('  ', $in) . trim($line) . PHP_EOL;
-    if($diff > 0)
-      $in += $diff;
+    $out .= str_repeat('  ', $in) . $line . PHP_EOL;
+    $in += $diff;
   }
   if($in > 0 && $debug)
     $out .= '<!-- INDENT ERR: in > 0 -->';
