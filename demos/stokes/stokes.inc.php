f712c74de74be1d33b034dbaeedbc83c52019f8f
Vašek Potoček <vasek.potocek@post.cz>
2020-05-06 02:33:54 +0200

    Stokes: description and tips

--- demos/stokes/stokes.inc.php
+++ demos/stokes/stokes.inc.php
@@ -13,18 +13,20 @@ $files[] = $demodir . '/flat.frag';
 $files[] = $demodir . '/line.vert';
 
 if($en) {
-  $desc = 'Description in English';
+  $desc = 'Stokes parameters describe the polarization state of an electromagnetic wave (in this demonstration only fully polarized waves are considered). They make a three-dimensional vector on the Poincaré sphere, which shows the topological relations between various trajectories of the electric field vector E. We can also characterize polarization by the amplitudes of oscillations of the E<sub>x</sub> a E<sub>y</sub> components and their phase difference, or via the geometry of the polarization ellipse.';
   $try = 'Tips for trying:';
   $tips = [
-    'tips'
+    'What parts of the Poincaré sphere correspond to linear, circular, elliptic polarizations?',
+    'How are the E<sub>x</sub> a E<sub>y</sub> oscillations affected by the spherical angles ϑ, ϕ?',
+    'Which parameters of the ellipse are conserved in rotations of the vector around x, y, z axes? (You can try that by tapping and holding the axis tips.)'
   ];
 } else {
-  $desc = 'Popis';
+  $desc = 'Stokesovy parametry popisují polarizační stav světelné vlny (zde v ukázce uvažujeme pouze zcela polarizované vlny). Tvoří trojrozměrný vektor ležící na tzv. Poincarého sféře. Ta ukazuje topologické vztahy mezi možnými trajektoriemi opisovanými vektorem elektrické intenzity E. Polarizaci také můžeme charakterizovat pomocí amplitud oscilací složek E<sub>x</sub> a E<sub>y</sub> a jejich fázového rozdílu či pomocí geometrických vlastností polarizační elipsy.';
   $try = 'Zkuste si:';
   $tips = [
     'Jakým částem Poincarého sféry odpovídají lineární polarizace? Kruhové? Eliptické?',
     'Jaký vliv na oscilace x- a y-složky elektrické intenzity mají sférické úhly ϑ, ϕ?',
-    'Jaké parametry trajektorie elektrické intenzity se zachovávají při rotacích Stokesových parametrů kolem x, y, z?'
+    'Jaké parametry trajektorie elektrické intenzity se zachovávají při rotacích Stokesových parametrů kolem x, y, z? (Stokesův vektor bude rotovat, stisknete-li a přidržíte špičky os.)'
   ];
 }
 
@@ -35,7 +37,7 @@ $tips = join(PHP_EOL, $list);
 
 print <<<HTML
 <h1>$demotitle</h1>
-$desc
+<p>$desc</p>
 <div class="row">
   <div class="container"><canvas id="sphere"></canvas></div>
   <div class="container">
